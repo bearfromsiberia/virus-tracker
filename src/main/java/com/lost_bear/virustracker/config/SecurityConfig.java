51486681ed652fff39b06aa5ebd95d1dfda43b99
Ilya Lomtev <lomteb38@gmail.com>
2024-03-06 18:46:16 +0700

    Посмотрел как работает InMemoryUserDetailsManager

--- src/main/java/com/lost_bear/virustracker/config/SecurityConfig.java
+++ src/main/java/com/lost_bear/virustracker/config/SecurityConfig.java
@@ -4,15 +4,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig{
 
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        return http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+//                authorizationManagerRequestMatcherRegistry.requestMatchers("/**").permitAll()).build();
+//    }
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
-                authorizationManagerRequestMatcherRegistry.requestMatchers("/**").permitAll()).build();
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager users(){
+        UserDetails user = User.builder().username("admin").password(new BCryptPasswordEncoder().encode("12345"))
+                .roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
--- src/main/java/com/lost_bear/virustracker/controllers/HomeController.java
+++ src/main/java/com/lost_bear/virustracker/controllers/HomeController.java
@@ -2,11 +2,18 @@ package com.lost_bear.virustracker.controllers;
 
 import com.lost_bear.virustracker.services.VirusDataService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 @RestController
 public class HomeController {
@@ -20,5 +27,10 @@ public class HomeController {
         model.addObject("locations",virusDataService.getAllStats());
         model.addObject("totalReportedCases",virusDataService.getTotal_deaths());
         return model;
+    }
+    @GetMapping("/hello")
+    public ResponseEntity<Map<String,String>> sayHello(){
+        UserDetails user = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(Map.of("message","Hello, %s".formatted(user.getUsername())));
     }
 }
